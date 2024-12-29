efaa9c8bcba73d7c31435d5f97de951673ca8d86
me <tkutlesa@gmail.com>
2024-12-28 22:36:19 -0500

    dotnet

--- lib/phoenix_web/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule PhoenixWeb.Mailer do
-  use Swoosh.Mailer, otp_app: :phoenix_web
-end
--- lib/phoenix_web/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule PhoenixWeb.Repo do
-  use Ecto.Repo,
-    otp_app: :phoenix_web,
-    adapter: Ecto.Adapters.Postgres
-end
